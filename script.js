c1d10688af06c1e8b7e4fa6b464a709d4eca9237
Nycolas <nybertocco@gmail.com>
2024-01-23 13:53:42 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,33 @@
+let firstNumber = prompt('Digite o primeiro número')
+ let secondNumber = prompt('Digite o segundo número')
+
+ firstNumber = Number(firstNumber)
+ secondNumber = Number(secondNumber)
+
+ const sum = firstNumber + secondNumber
+ const sub = firstNumber - secondNumber
+ const multi = firstNumber * secondNumber
+ const div = firstNumber / secondNumber
+ const restDiv = firstNumber % secondNumber
+
+ alert('Soma: ' + sum)
+ alert('Subtração: ' + sub)
+ alert('Multiplicação: ' + multi)
+ alert('Divisão: ' + div)
+ alert('Resto da divisão: ' + restDiv)
+
+ if (sum % 2 === 0){
+   alert("A soma é par")
+ }
+
+ else {
+   alert("A soma é ímpar")
+ }
+
+ if (firstNumber === secondNumber) {
+   alert("Os números são iguais")
+ }
+
+ else {
+   alert("Os números são diferentes")
+ }
